1bc441a958fa59edb5ce7b0785e336cd0eee9dc3
Satoshi Innami <innamisatoshi@gmail.com>
2019-01-24 20:52:55 +0900

    initial commit

---
+++ app/layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-toolbar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+    </v-toolbar>
+    <v-content>
+      <v-container fill-height>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      title: 'Daily App'
+    }
+  }
+}
+</script>
---
+++ app/pages/dailyapp.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout
+    fill-height
+    justify-center
+    align-center>
+    <v-flex xs12 lg6>
+      <div class="text-xs-center">
+        <span class="headline">100日で一歩上の開発者へ</span>
+      </div>
+      <div class="mail-panel">
+        <v-text-field
+          label="メールアドレスを入力"
+I         append-outer-icon="mail"
+          outline
+          @click:append-outer="sendMail()"
+        >
+        </v-text-field>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+
+  },
+  methods: {
+    sendMail () {
+      console.log("sent email")
+    }
+  }
+}
+</script>
+
+<style>
+.v-icon {
+  font-size: 35px;
+}
+</style>
